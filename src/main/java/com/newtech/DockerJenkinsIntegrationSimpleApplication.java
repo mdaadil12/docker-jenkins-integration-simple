f69889d4483e684750610309974e78f56ede6ffa
Md Aadil Ansari <a4aadilansari@gmail.com>
2022-02-09 12:36:04 +0530

    first commit

---
+++ src/main/java/com/newtech/DockerJenkinsIntegrationSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.newtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsIntegrationSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsIntegrationSimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newtech/DockerJenkinsIntegrationSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsIntegrationSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
